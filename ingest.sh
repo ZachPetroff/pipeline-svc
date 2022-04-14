bd681eba1bf2e2595e98438abf902ce501825cf6
Zachary Petroff <59905848+ZachPetroff@users.noreply.github.com>
2022-04-14 05:27:41 -0400

    Add files via upload

---
+++ ingest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install wget
+
+# remove previous csv file
+rm *.csv
+
+# download new data 
+echo Enter Data URL: 
+
+read data 
+
+wget data -O data.csv 
---
+++ install_missing.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+sudo apt-get update
+sudo apt-get -y -qq --fix-missing install python3-numpy python3-pandas python3-sklearn python3-matplotlib python3-sys
